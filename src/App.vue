eeaaa1a2549f6e2faa6f49ab17a4c4f47701b2cd
DerZade <DerZade@gmail.com>
2019-05-04 11:18:25 +0200

    Project scaffold

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+    <div id="app">
+    </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+
+@Component
+export default class App extends Vue {}
+</script>
+
+<style lang="scss">
+html, body, #app {
+  height: 100%;
+  margin: 0;
+}
+</style>
